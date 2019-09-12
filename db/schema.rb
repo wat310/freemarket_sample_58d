cf028abc1f3ce7dbb79333ad7de1b7f08b344d06
wat310 <midtilda_velca@yahoo.co.jp>
2019-09-12 21:35:40 +0900

    prefectureテーブル作成→削除
    都道府県を作成するgemがあったため、わざわざテーブルを用意する必要がなくなった。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_035150) do
+ActiveRecord::Schema.define(version: 2019_09_12_123152) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
