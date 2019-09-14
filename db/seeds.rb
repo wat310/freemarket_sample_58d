5f480b171d47ec033ba44cde5bf97d7235f11851
wat310 <midtilda_velca@yahoo.co.jp>
2019-09-14 17:03:13 +0900

    seeds/category
    categoryテーブルの初期データを登録するためのseedファイルを作成。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,2 @@
-# category等の初期データの記述
-#メンズ
-men = Category.create(name: "メンズ")
-men_tops = men.children.create(name: "トップス")
-men_tops.children.create([{name: "Tシャツ/カットソー(半袖/袖なし)"}])
+# rake db:seedの時に一緒に読み込むための記述
+require './db/seeds/category.rb'
