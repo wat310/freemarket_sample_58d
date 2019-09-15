0869aa00cb45f3b4c27579ad4014dfe62ade4463
shiwori <grmrgzm05@gmail.com>
2019-09-15 21:31:38 +0900

    add_gem_and_image-upload

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,11 +12,14 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
- # Basic認証
+ # Basic認証/bashからenvに変更したのでコメントアウト中
 set :default_env, {
   # BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
   # BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
 }
+
+#secrets.ymlではリリースバージョン間でシンボリックリンクにして共有→masterkeyに置き換え
+append :linked_files, 'config/database.yml', 'config/master.key'
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -24,6 +27,14 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+
+# 画像アップロード/環境変数をcapistranoでの自動デプロイで利用
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
