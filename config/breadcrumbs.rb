b5ca84fa5dc068295fe385f188035af27866cdb0
mtsunori <mickmc.3232@i.softbank.jp>
2019-09-27 21:16:37 +0900

    パンくず実装のための一次記述

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,77 @@
+crumb :root do
+  link "メルカリ", root_path
+end
+
+crumb :mypage do
+  link "マイページ", mypage_index_path
+  parent :root
+end
+
+crumb :logout do
+  link "ログアウト"
+  parent :mypage
+end
+
+crumb :identification do
+  link "本人情報の確認"
+  parent :user_mypage
+end
+
+crumb :item_show do |item|
+  link item.name, item_path(item.id)
+  parent :root
+end
+
+crumb :wallet do
+  link "支払い方法" 
+  parent :user_mypage
+end
+
+crumb :wallet_paying do
+  link "支払い方法" 
+  parent :user_mypage
+end
+
+crumb :wallet_show do
+  link "支払い方法" 
+  parent :user_mypage
+end
+
+crumb :profile do
+  link "プロフィール" 
+  parent :user_mypage
+end
+
+crumb :search do |q|
+  link q.name_cont
+  parent :root
+end
+
+crumb :sell_item do |q|
+  link "出品した商品"
+  parent :user_mypage
+end
+# crumb :projects do
+#   link "Projects", projects_path
+# end
+
+# crumb :project do |project|
+#   link project.name, project_path(project)
+#   parent :projects
+# end
+
+# crumb :project_issues do |project|
+#   link "Issues", project_issues_path(project)
+#   parent :project, project
+# end
+
+# crumb :issue do |issue|
+#   link issue.title, issue_path(issue)
+#   parent :project_issues, issue.project
+# end
+
+# If you want to split your breadcrumbs configuration over multiple files, you
+# can create a folder named `config/breadcrumbs` and put your configuration
+# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
+# folder are loaded and reloaded automatically when you change them, just like
+# this file (`config/breadcrumbs.rb`).
