0ccbe283c55e9bd346cc286a3b4185b5f263c3c4
mtsunori <mickmc.3232@i.softbank.jp>
2019-09-29 20:41:02 +0900

    サムネイルイメージの透明度の設定記述・CSS側不要記述の削除

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -3,12 +3,15 @@ $(document).on('turbolinks:load', function(){
   var large_item_box = $('.detail__box__item__image--large');
   var small_item_box = $('.detail__box__item__image--small');
   var small_item_src;
-
+  //マウス移動、サムネイル上でイベント発火
   small_item_box.children('img').on('mouseover',function(){
+
   //画像の切り替え機能
   small_item_src = $(this).attr('src')
   large_item_box .children('img').attr('src',small_item_src)
-})
 
-
+  //画像を選んだ際の透明度の設定
+  small_item_box.children('img').css('opacity', '0.4');
+  $(this).css('opacity', '1.0');
+  })
 });
