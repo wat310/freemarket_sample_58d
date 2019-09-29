58ff0e3adf674c3e301452e4abc5bf355d2ebcb3
HiroakiMorita <taiyou3291@outlook.jp>
2019-09-29 21:21:07 +0900

    バリデーション実装（修正）

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,11 +1,8 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user, optional: true # userのバリデーション外す
 
-  validates :user_id,
-  presence: true
-  validates :uid,
-  presence: true
-  validates :provider,
-  presence: true
+  validates :user_id, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
 
 end
