caeda785858bac4a475603800b02881c40724bf4
shiwori <grmrgzm05@gmail.com>
2019-09-29 12:52:38 +0900

    pay登録・一覧・削除
    一旦、一通り実装。
    購入機能に関しては昨日マージされた購入ページとつなぐため、コメントアウト中。

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -23,7 +23,7 @@ class CardController < ApplicationController
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       # binding.pry
       if @card.save
-        redirect_to action: "show"       
+        redirect_to action: "show"
       else
         redirect_to action: "pay"
       end
