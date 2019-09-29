773251d8c427bbe87123888ec5a0f931423a8bba
HiroakiMorita <taiyou3291@outlook.jp>
2019-09-29 21:07:12 +0900

    バリデーション実装

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,11 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user, optional: true # userのバリデーション外す
+
+  validates :user_id,
+  presence: true
+  validates :uid,
+  presence: true
+  validates :provider,
+  presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
+        validates :nickname, presence: true, length: { maximum: 20 }
+        VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+        validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+        validates :family_name_kanji, presence: true, length: { maximum: 20 }
+        validates :first_name_kanji, presence: true, length: { maximum: 20 }
+        validates :family_name_kana, presence: true, length: { maximum: 20 }
+        validates :first_name_kana, presence: true, length: { maximum: 20 }
+        validates :password, presence: true, length: { minimum: 7, maximum: 15}
+        validates :phone_number, presence: true
+        validates :birth_year, presence: true
+        validates :birth_month, presence: true
+        validates :birth_day, presence: true
+        validates :postal_code, presence: true
+        validates :prefecture_id, presence: true
+        validates :city, presence: true
+        validates :house_number, presence: true
+
         # SNSログイン機能。facebookかgoogleからユーザー情報を引っ張り出してreturnする。
         # このときSNScredentialsテーブルにはuidとproviderを保存するが、User情報は保存しない。
          def self.find_oauth(auth)
@@ -37,59 +54,9 @@ class User < ApplicationRecord
           return {  user: user , sns: sns } # hashでsnsのidを返り値として保持しておく
         end
 
-        
-
-        # TODO: 後でバリデーション機能を追加・修正予定。（SNS認証機能追加後）
-
-        # validates :nickname,
-        #   presence: true,
-        #   length: { maximum: 20 }
-        # validates :email,
-        #   presence: true,
-        #   uniqueness: { message: "メールアドレスに誤りがあります。ご確認いただき、正しく変更してください。" },
-        #   format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*[a-zA-Z]+\z/, allow_blank: true, message: "フォーマットが不適切です" }
-        # validates :family_name_kanji,
-        #   presence: true,
-        #   length: { maximum: 35 }
-        # validates :first_name_kanji,
-        #   presence: true,
-        #   length: { maximum: 35 }
-        # validates :family_name_kana,
-        #   presence: true,
-        #   length: { maximum: 35 }
-        # validates :first_name_kana,
-        #   presence: true,
-        #   length: { maximum: 35 }
-        # validates :password,
-        #   presence: true,
-        #   length: { minimum: 7, maximum: 15}
-        # validates :phone_number,
-        #   length: { maximum: 11, message: "11文字で入力して下さい" },
-        #   uniqueness: { message: "この電話番号は既に登録されています。" },
-        #   format: { with: /\A\d{11}\z/, message: "この電話番号は登録できません" }
-        # validates :birth_year,
-        #   presence: true
-        # validates :birth_month,
-        #   presence: true
-        # validates :birth_day,
-        #   presence: true
-        # validates :postal_code,
-        #   presence: true
-        # validates :prefecture_id,
-        #   presence: true
-        # validates :city,
-        #   presence: true
-        # validates :house_number,
-        #   presence: true
-        # validates :building,
-        #   presence: true
-
-
         extend ActiveHash::Associations::ActiveRecordExtensions
         belongs_to_active_hash :prefecture
         
-        #  :recoverable, :rememberable, :validatable
-
   has_many :items, dependent: :destroy
   has_many :sns_credentials, dependent: :destroy
 
