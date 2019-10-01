de94ceeb579bbbf021f2a054f045b3b086d0d6de
shiwori <grmrgzm05@gmail.com>
2019-10-01 18:15:14 +0900

    DB関連でデプロイエラー解決のため

--- db/migrate/20191001083952_drop_credit_cards.rb
+++
@@ -1,5 +0,0 @@
-class DropCreditCards < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :credit_cards
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_083952) do
+ActiveRecord::Schema.define(version: 2019_09_26_052441) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
