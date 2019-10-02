c6d88836b5e66661499033f3c0d64f773d8e7db6
shiwori <grmrgzm05@gmail.com>
2019-10-02 11:41:37 +0900

    エラー解消のためcredential修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,35 +18,6 @@ set :keep_releases, 5
 #   # BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
 # }
 
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-# task :restart do
-# # invoke 'unicorn:restart'
-# invoke 'unicorn:stop'
-# invoke 'unicorn:start'
-# end
-# end
-
-#restartコード
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#  task :restart do
-#    invoke 'unicorn:restart'
-#  end
-#  desc 'upload master.key'
-#  task :upload do
-#    on roles(:app) do |host|
-#      if test "[ ! -d #{shared_path}/config ]"
-#        execute "mkdir -p #{shared_path}/config"
-#      end
-#      upload!("config/master.key", "#{shared_path}/config/master.key")
-#    end
-#  end
-#  before :starting, 'deploy:upload'
-#  after :finishing, 'deploy:cleanup'
-# end
-
-
 #secrets.ymlではリリースバージョン間でシンボリックリンクにして共有→masterkeyに置き換え
 # set :linked_files, 'config/database.yml', 'config/master.key'
 set :linked_files, %w{config/master.key}
