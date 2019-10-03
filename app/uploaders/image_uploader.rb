8ee730721b0324435470e48ced3c968bd46d82ff
shiwori <grmrgzm05@gmail.com>
2019-10-04 00:37:03 +0900

    本番環境エラー検証
    デプロイできていた際は記述前のためいったんコメントアウト

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,9 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # 画像調整
   process resize_to_fit: [800, 800]
-  def extension_white_list
-    %w(jpg jpeg png)
-  end
+  # def extension_white_list
+  #   %w(jpg jpeg png)
+  # end
 
   # 環境ごとに保存先変更
   if Rails.env.development? || Rails.env.test?
